e6c88289d1ddddc3539ca49a59482169cba3dfce
andres <spokaro21@gmail.com>
2019-11-04 12:02:41 -0500

    9.2 complete

---
+++ Snacktacular/Spots.swift
@@ -0,0 +1,14 @@
+//
+//  Spots.swift
+//  Snacktacular
+//
+//  Created by Andrew Boucher on 11/4/19.
+//  Copyright © 2019 John Gallaugher. All rights reserved.
+//
+
+import Foundation
+
+class Spots {
+    var spotArray = [Spot]()
+    
+}
--- Snacktacular/SpotsListViewController.swift
+++ Snacktacular/SpotsListViewController.swift
@@ -12,8 +12,45 @@ import CoreLocation
 class SpotsListViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
+    var spots: Spots!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        spots = Spots()
+        
+        spots.spotArray.append(Spot(name: "El Pelon", address: "Comm. Ave", coordinate: CLLocationCoordinate2D(), averageRating: 0.0, numberOfReviews: 0, postingUserID: "", documentID: ""))
+        spots.spotArray.append(Spot(name: "Shake Shack", address: "The Street - Chesnut Hill", coordinate: CLLocationCoordinate2D(), averageRating: 0.0, numberOfReviews: 0, postingUserID: "", documentID: ""))
+        spots.spotArray.append(Spot(name: "Pino's Pizza", address: "Cleaveland Circle", coordinate: CLLocationCoordinate2D(), averageRating: 0.0, numberOfReviews: 0, postingUserID: "", documentID: ""))
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ShowSpot" {
+            let destination = segue.destination as! SpotDetailViewController
+            let selectedPathIndex = tableView.indexPathForSelectedRow!
+            destination.spot = spots.spotArray[selectedPathIndex.row]
+        } else {
+            if let selectedIndexPath = tableView.indexPathForSelectedRow {
+                tableView.deselectRow(at: selectedIndexPath, animated: true)
+            }
+        }
     }
 }
+extension SpotsListViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return spots.spotArray.count
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! SpotsTableViewCell
+        cell.nameLabel.text = spots.spotArray[indexPath.row].name
+        return cell
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+    }
+}
+
